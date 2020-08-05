33f0bdff0894edc98639b9d34bc9867a673b3207
Eddson Rengifo <eddsonrt@hotmail.com>
2020-08-05 17:02:49 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Login-with-react
-
-![](src/img/login-1366px.PNG)
-![](src/img/login-600px.PNG)
---
+++ precache-manifest.4e421451aaca99879de88948d82972a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937e4d7819019bfb8249b95f18a607a2",
+    "url": "/Login-with-react/index.html"
+  },
+  {
+    "revision": "953f05942c96e7071ede",
+    "url": "/Login-with-react/static/css/2.fe57fc5c.chunk.css"
+  },
+  {
+    "revision": "d5b0b5181540c0b97392",
+    "url": "/Login-with-react/static/css/main.e11579d4.chunk.css"
+  },
+  {
+    "revision": "953f05942c96e7071ede",
+    "url": "/Login-with-react/static/js/2.76715306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Login-with-react/static/js/2.76715306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b0b5181540c0b97392",
+    "url": "/Login-with-react/static/js/main.d97b2c9c.chunk.js"
+  },
+  {
+    "revision": "3df96653f4551b68f81c",
+    "url": "/Login-with-react/static/js/runtime-main.7a130f6d.js"
+  },
+  {
+    "revision": "d8bd756cdd8089771fcf960a46ce0943",
+    "url": "/Login-with-react/static/media/icon.d8bd756c.png"
+  }
+]);
